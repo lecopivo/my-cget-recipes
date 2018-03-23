88bcfd6cf42ebcec65507d2fe91d22fcbbf3826d
Tomas SKRIVAN <tskrivan@ldchwo008.ist.local>
2018-03-23 17:31:34 +0100

    changed imgui

--- etc/cget/recipes/imgui.v1.53/build.cmake
+++ etc/cget/recipes/imgui.v1.53/build.cmake
@@ -1,8 +1,8 @@
 cmake_minimum_required(VERSION 2.8)
 
 add_library(ImGui imgui.cpp imgui_draw.cpp imgui_demo.cpp)
-set(INCLUDE_FILES imconfig.h imgui.h stb_rect_pack.h stb_textedit.h stb_truetype.h)
+set(INCLUDE_FILES imconfig.h imgui.h)
 
 install(TARGETS ImGui DESTINATION "lib/")
-install(FILES ${INCLUDE_FILES} DESTINATION "include/imgui")
+install(FILES ${INCLUDE_FILES} DESTINATION "include/")
 
