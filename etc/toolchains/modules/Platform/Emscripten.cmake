a5bd366e1831cc3e5058fffb8a21b7addd30c65b
Tomas SKRIVAN <tskrivan@ldchwo008.ist.local>
2018-03-23 12:38:28 +0100

    changed default web executable from ".js" to ".html"

--- etc/toolchains/modules/Platform/Emscripten.cmake
+++ etc/toolchains/modules/Platform/Emscripten.cmake
@@ -8,7 +8,7 @@ include(Platform/Linux)
 # Prefixes/suffixes for building
 set(CMAKE_STATIC_LIBRARY_PREFIX "")
 set(CMAKE_STATIC_LIBRARY_SUFFIX ".bc")
-set(CMAKE_EXECUTABLE_SUFFIX ".js")
+set(CMAKE_EXECUTABLE_SUFFIX ".html")
 
 # Prefixes/suffixes for finding libraries
 set(CMAKE_FIND_LIBRARY_PREFIXES "")
